04a2709e834b3fd76c516b5ee4df1438eafa50e4
CompulsiveCoder <compulsivecoder@gmail.com>
2016-02-28 19:08:26 +1100

    Added scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+cd mod/ipfs-echo/
+sh build.sh
+cd ../..
+
+xbuild src/ipfs.post.sln /p:Configuration=Release
---
+++ run-from-github.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/CompulsiveCoder/ipfs-post.git
+cd ipfs-post
+sh init-build-run.sh
