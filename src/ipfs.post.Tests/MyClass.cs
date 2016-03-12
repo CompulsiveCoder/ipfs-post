752cf54fec37382b3bb00979ddabb25dae04abd4
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 13:52:10 +1100

    Fixed testing

---
+++ src/ipfs.post.Tests/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ipfs.post.Tests
+{
+	[TestFixture]
+	public class HelloTest
+	{
+		[Test]
+		public void Test ()
+		{
+			// TODO: Implement testing
+			Console.WriteLine ("Hello world");
+		}
+	}
+}
+
