9c28913d342eb1997137f31b0ffa21ff60ce9ff1
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 18:07:32 +1100

    Script updates

--- run-from-github-via-docker.sh
+++ run-from-github-via-docker.sh
@@ -1,2 +1,12 @@
-docker run -it compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/master/run-from-github.sh | sh"
+BRANCH=$1
+
+if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
+    BRANCH="master"
+fi
+
+docker run -it -p 8081:8081 compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/run-from-github.sh | sh -s $BRANCH"
 
--- run-ipfs.sh
+++ run-ipfs.sh
@@ -1,5 +1,11 @@
 DIR=src/ipfs.post.WWW/ipfs-data
-mkdir $DIR
+
+
+if [ ! -d "$DIR" ];
+then
+    mkdir $DIR
+fi
+
 cd $DIR
 /usr/local/bin/ipfs init -f
 /usr/local/bin/ipfs daemon
