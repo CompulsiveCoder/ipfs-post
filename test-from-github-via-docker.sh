d174d71e883ce8bcfb0224941b75b5b750cbd85a
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 15:32:26 +1100

    Fixing test scripts

--- test-from-github-via-docker.sh
+++ test-from-github-via-docker.sh
@@ -1,7 +1,11 @@
 BRANCH=$1
 
 if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
-docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/test-from-github.sh | sh -s $BRANCH"
+docker run -it compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/test-from-github.sh | sh -s $BRANCH"
--- test-from-github.sh
+++ test-from-github.sh
@@ -8,4 +8,7 @@ echo "Branch: $BRANCH"
 
 git clone https://github.com/CompulsiveCoder/ipfs-post.git --branch $BRANCH
 cd ipfs-cs
+
+git submodule update --init --recursive
+
 sh init-build-test.sh
