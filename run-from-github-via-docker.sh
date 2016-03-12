e223e617c373d6159e8c84f21514f1513335a482
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 15:58:44 +1100

    Changed docker image

--- run-from-github-via-docker.sh
+++ run-from-github-via-docker.sh
@@ -1,1 +1,1 @@
-docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "apt-get update && apt-get install -y curl git && curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/master/run-from-github.sh | sh"
+docker run -it compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "apt-get update && apt-get install -y curl git && curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/master/run-from-github.sh | sh"
