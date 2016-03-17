8849fa652ce6b549b107ef2b87e17ddf95f05fc9
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-17 22:41:33 +1100

    Tweaked prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,12 @@
 sudo apt-get update
 sudo apt-get install -y git wget mono-complete mono-xsp4 unzip
 
+DIR=$PWD
+
 wget -q https://raw.githubusercontent.com/ipfs/install-go-ipfs/master/install-ipfs.sh
 sudo sh install-ipfs.sh
 
 mozroots --import --sync
+
+cd $DIR
 
