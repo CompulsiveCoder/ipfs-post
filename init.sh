89edc2a92ad9a9e579d829f939ed67a79a8c9e45
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-18 09:38:28 +1100

    Added/updated scripts

--- init.sh
+++ init.sh
@@ -6,8 +6,9 @@ echo "Dir: $PWD"
 cd mod/ipfs-echo/
 INIT_FILE="init.sh"
 if [ ! -f "$INIT_FILE" ]; then
-  echo "ipfs-cs init file not found: $PWD/$INIT_FILE. Did the submodule fail to initialize?"
+  echo "ipfs-echo init file not found: $PWD/$INIT_FILE. Did the submodule fail to initialize?"
 else
+  echo "ipfs-echo submodule found"
   sh init.sh
   cd $DIR
 
