6d63e3e6346cdb71dd6b5d72be4d3c2627bf6d57
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-17 22:52:46 +1100

    Tweaked script

--- prepare-and-test-from-github.sh
+++ prepare-and-test-from-github.sh
@@ -10,5 +10,5 @@ fi
 
 echo "Branch: $BRANCH"
 
-curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/prepare.sh | sh
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/prepare.sh | sh && \
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/test-from-github.sh | sh -s $BRANCH
