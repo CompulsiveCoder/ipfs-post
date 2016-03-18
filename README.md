19cbf4cc0f6d64322a9710a4f30121625d0274d2
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-18 11:42:50 +1100

    Tweaked run via docker script

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # ipfs-post
 A simple way to post data to restricted folders on ipfs.
 
-To run:
+To run...
+
+
+Standard:
 
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/master/run-from-github.sh | sh
+
+Docker:
+
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/master/run-from-github-via-docker.sh | sh
--- run-from-github-via-docker.sh
+++ run-from-github-via-docker.sh
@@ -10,4 +10,4 @@ fi
 
 echo "Branch: $BRANCH"
 
-docker run -it -p 8081:8081 compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/run-from-github.sh | sh -s $BRANCH"
+docker run -i -p 8081:8081 compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-post/$BRANCH/run-from-github.sh | sh -s $BRANCH"
